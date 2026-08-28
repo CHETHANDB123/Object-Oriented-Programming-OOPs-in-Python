3a21e58a005c95a03b7e66971331d65de4e24750
CHETHAN D B <chethandb.cse@gmail.com>
2026-08-28 15:32:00 +0530

    from | import keyword

---
+++ 1.1_2.py
@@ -0,0 +1,22 @@
+'''
+import keyword as k,math as m,random
+
+# import math
+# import random
+print(k.kwlist)
+print(m.sqrt(25))
+print(m.pow(2,5))
+print(random.randint(11,19))
+
+#from
+from math import sqrt,pow,cbrt,factorial  #keywords=from,import   #module=math
+print(sqrt(25))
+print(pow(2,5))
+print(cbrt(1000))
+print(factorial(5))
+'''
+from package2.file2 import rapper,birthday,dispaly,showcase
+print(rapper)
+print(birthday)
+dispaly()
+showcase()
