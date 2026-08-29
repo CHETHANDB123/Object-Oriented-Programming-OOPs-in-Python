0ded191d02078f40d8acee49eb2fd76d4be4d9ae
CHETHAN D B <chethandb.cse@gmail.com>
2026-08-29 22:59:14 +0530

    From Package Name . File Name import Function Name

---
+++ 1.1_7.py
@@ -0,0 +1,10 @@
+from package3.calculate import multiply
+def calculate_squarearea(side):
+    #print(side*side)
+    print(multiply(side,side))
+def calculate_rectanglearea(l,b):
+    #print(l*b)
+    print(multiply(l,b))
+calculate_squarearea(5)
+calculate_rectanglearea(2,2)
+
