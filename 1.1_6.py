73459fcde5fce15267384fd046d8db9312599700
CHETHAN D B <chethandb.cse@gmail.com>
2026-08-29 22:49:57 +0530

    File 3

---
+++ 1.1_6.py
@@ -0,0 +1,11 @@
+# print(__file__)
+# print(__package__)
+# print(__cached__)
+# 'this is documentation string of the file'
+# print(__doc__)
+
+def display():
+    'this function is used to display weekday'
+    print("today is thursday")
+
+print(display.__doc__) #function level doc
